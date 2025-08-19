4ba11ae141ce7025646cc4b70de6ebceef7388a5
mingki <myong1306@gmail.com>
2025-08-20 02:27:09 +0900

    [feat] : 상품 등록할 때 사진, 거래 방식 추가 - 사진 최대 10장 등록 가능, Firebase Storage에 저장 - 판매하기, 나눔하기 선택 가능

--- src/main/java/com/example/recycling_app/dto/market/ProductDto.java
+++ src/main/java/com/example/recycling_app/dto/market/ProductDto.java
@@ -11,5 +11,6 @@ public class ProductDto {
     private String productDescription; // 제품 설명
     private int price; // 제품 가격
     private List<String> images; // 이미지 URL 목록
-    private long createdAt; // 생성 시간 (Timestamp)
+    private long createdAt; // 생성 시간
+    private String transactionType; // 거래 방식 (판매하기, 나눔하기)
 }
