90c21d017caafb70c8714a83bf17dfa17035691d
mingki <myong1306@gmail.com>
2025-08-23 16:19:23 +0900

    AWS EC2에서 이미지를 못 불러오는 오류 수정 테스트 과정

--- src/main/java/com/example/recycling_app/config/FirebaseInitializer.java
+++ src/main/java/com/example/recycling_app/config/FirebaseInitializer.java
@@ -6,24 +6,31 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.StorageClient;
 import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 @Configuration
 public class FirebaseInitializer {
 
+    @Value("${firebase.sdk.path}")
+    private String firebaseSdkPath;
+
+    @Value("${firebase.storage.bucket}")
+    private String firebaseStorageBucket;
+
     @PostConstruct
     public void init() {
         try {
-            FileInputStream serviceAccount =
-                    new FileInputStream("src/main/resources/firebase/firebase-service-account.json");
+            InputStream serviceAccount = new ClassPathResource(firebaseSdkPath).getInputStream();
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setStorageBucket("your-name-382bf.firebasestorage.app")
+                    .setStorageBucket(firebaseStorageBucket)
                     .build();
 
             if (FirebaseApp.getApps().isEmpty()) {
@@ -31,12 +38,12 @@ public class FirebaseInitializer {
                 System.out.println("Firebase initialized");
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException("Firebase 초기화 중 오류 발생", e);
         }
     }
 
     @Bean
     public Storage storage() {
-        return StorageClient.getInstance().bucket().getStorage();
+        return StorageClient.getInstance().bucket(firebaseStorageBucket).getStorage();
     }
 }
